498305ef115838183c2ffc2aabde3ad6e84b9026
igor-perlinnet <igor@perlin.net>
2023-08-17 09:00:18 +0300

    Remove blockchain layer temporarily

--- backend/services/src/ledger-replicator/perl-ledger.service.ts
+++ backend/services/src/ledger-replicator/perl-ledger.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { Wallet, Contract, JsonRpcProvider } from 'ethers';
+// import { Wallet, Contract, JsonRpcProvider } from 'ethers';
 import { ConfigService } from "@nestjs/config";
 import { CreditOverall } from "../shared/entities/credit.overall.entity";
 import { PerlLedgerConfig } from "../shared/dto/perlLedger.config";
@@ -8,7 +8,7 @@ import contractABI from './PerlLedger.json';
 
 @Injectable()
 export class PerlLedgerService {
-  private contract: Contract;
+  // private contract: Contract;
 
   constructor(
     private logger: Logger,
@@ -16,9 +16,9 @@ export class PerlLedgerService {
     configService: ConfigService
   ) {
     const config = configService.get<PerlLedgerConfig>('distributedLedger');
-    const provider = new JsonRpcProvider(config.providerURL);
-    const wallet = new Wallet(config.walletPrivateKey, provider);
-    this.contract = new Contract(config.contractAddress, contractABI as any, wallet)
+    // const provider = new JsonRpcProvider(config.providerURL);
+    // const wallet = new Wallet(config.walletPrivateKey, provider);
+    // this.contract = new Contract(config.contractAddress, contractABI as any, wallet)
 
     logger.log('Constructor initialized', 'PerlLedgerService', config);
   }
@@ -35,11 +35,11 @@ export class PerlLedgerService {
       }
 
       const { txId, txRef, txType, credit } = overall;
-      const txResponse = await this.contract.createCreditOverall(txId, txRef, txType, credit);
-      await txResponse.wait();
+      // const txResponse = await this.contract.createCreditOverall(txId, txRef, txType, credit);
+      // await txResponse.wait();
 
-      hash = txResponse.hash;
-      this.logger.debug('Response received', 'createLedgerRecord', txResponse);
+      // hash = txResponse.hash;
+      // this.logger.debug('Response received', 'createLedgerRecord', txResponse);
     } catch (error) {
       this.logger.error('Error occured', 'createLedgerRecord', error);
     }
