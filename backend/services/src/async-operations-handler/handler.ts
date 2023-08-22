13f55034daa33113387b37e92bf5c2e054f6ec83
igor-perlinnet <igor@perlin.net>
2023-08-22 11:35:16 +0300

    Restore logger

--- backend/services/src/async-operations-handler/handler.ts
+++ backend/services/src/async-operations-handler/handler.ts
@@ -2,12 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { Handler, Context } from "aws-lambda";
 import { AsyncOperationsHandlerInterface } from "./async-operations-handler-interface.service";
 import { AsyncOperationsModule } from "./async-operations.module";
+import { getLogger } from "src/shared/server";
 
 export const handler: Handler = async (event: any, context: Context) => {
   const app = await NestFactory.createApplicationContext(
     AsyncOperationsModule,
     {
-      logger: console,
+      logger: getLogger(AsyncOperationsModule),
     }
   );
 
