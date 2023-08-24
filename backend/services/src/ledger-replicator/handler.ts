c33d5ed63e6a270167f9eed1e143be8bf4d520fb
igor-perlinnet <igor@perlin.net>
2023-08-24 06:05:12 +0300

    Use standard logger for replicator

--- backend/services/src/ledger-replicator/handler.ts
+++ backend/services/src/ledger-replicator/handler.ts
@@ -2,12 +2,12 @@
 import { Handler, Context } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { LedgerReplicatorModule } from './ledger-replicator.module';
-import { getLogger } from '../shared/server';
+// import { getLogger } from '../shared/server';
 import { LedgerReplicatorInterface } from './replicator-interface.service';
 
 export const handler: Handler = async (event: any, context: Context) => {
    const app = await NestFactory.createApplicationContext(LedgerReplicatorModule, {
-      logger: getLogger(LedgerReplicatorModule),
+      logger: console, // getLogger(LedgerReplicatorModule),
     });
     await app.get(LedgerReplicatorInterface).replicate(event);
 }
