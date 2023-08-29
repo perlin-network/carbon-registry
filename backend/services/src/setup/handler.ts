57fcc17f64190b18f37f7072d325e683bfa1be83
igor-perlinnet <igor@perlin.net>
2023-08-29 05:35:13 +0300

    Enhance location svc to persist regions into db

--- backend/services/src/setup/handler.ts
+++ backend/services/src/setup/handler.ts
@@ -1,27 +1,27 @@
 import { NestFactory } from "@nestjs/core";
-import { Role } from "../shared/casl/role.enum";
-import { UserDto } from "../shared/dto/user.dto";
-import { LedgerDbModule } from "../shared/ledger-db/ledger-db.module";
+// import { Role } from "../shared/casl/role.enum";
+// import { UserDto } from "../shared/dto/user.dto";
+// import { LedgerDbModule } from "../shared/ledger-db/ledger-db.module";
 import { getLogger } from "../shared/server";
-import { UtilModule } from "../shared/util/util.module";
-import { Country } from "../shared/entities/country.entity";
-import { CountryService } from "../shared/util/country.service";
-import { CreditOverall } from "../shared/entities/credit.overall.entity";
-import { CompanyModule } from "../shared/company/company.module";
-import { OrganisationDto as OrganisationDto } from "../shared/dto/organisation.dto";
-import { CompanyRole } from "../shared/enum/company.role.enum";
-import { CompanyService } from "../shared/company/company.service";
-import { UserModule } from "../shared/user/user.module";
-import { UserService } from "../shared/user/user.service";
-import { TxType } from "../shared/enum/txtype.enum";
-import { LedgerDBInterface } from "../shared/ledger-db/ledger.db.interface";
+// import { UtilModule } from "../shared/util/util.module";
+// import { Country } from "../shared/entities/country.entity";
+// import { CountryService } from "../shared/util/country.service";
+// import { CreditOverall } from "../shared/entities/credit.overall.entity";
+// import { CompanyModule } from "../shared/company/company.module";
+// import { OrganisationDto as OrganisationDto } from "../shared/dto/organisation.dto";
+// import { CompanyRole } from "../shared/enum/company.role.enum";
+// import { CompanyService } from "../shared/company/company.service";
+// import { UserModule } from "../shared/user/user.module";
+// import { UserService } from "../shared/user/user.service";
+// import { TxType } from "../shared/enum/txtype.enum";
+// import { LedgerDBInterface } from "../shared/ledger-db/ledger.db.interface";
 import { Handler } from "aws-lambda";
 import { LocationModule } from "../shared/location/location.module";
 import { LocationInterface } from "../shared/location/location.interface";
-import { ProgrammeModule } from "../shared/programme/programme.module";
-import { ProgrammeService } from "../shared/programme/programme.service";
-import { ConfigService } from "@nestjs/config";
-const fs = require("fs");
+// import { ProgrammeModule } from "../shared/programme/programme.module";
+// import { ProgrammeService } from "../shared/programme/programme.service";
+// import { ConfigService } from "@nestjs/config";
+// const fs = require("fs");
 
 export const handler: Handler = async (event) => {
   console.log(`[setup] Handler Started with %j`, event);
@@ -30,215 +30,215 @@ export const handler: Handler = async (event) => {
     event = process.env;
   }
 
-  let userService: UserService;
+  // let userService: UserService;
 
-  try {
-    const userApp = await NestFactory.createApplicationContext(UserModule, {
-      logger: getLogger(UserModule),
-    });
-    userService = userApp.get(UserService);
-    console.log(`[setup] User module initialized`);
-  } catch (e) {
-     console.error("[setup] User module failed to initialize, stopping.", e);
-     return;
-  }
+  // try {
+  //   const userApp = await NestFactory.createApplicationContext(UserModule, {
+  //     logger: getLogger(UserModule),
+  //   });
+  //   userService = userApp.get(UserService);
+  //   console.log(`[setup] User module initialized`);
+  // } catch (e) {
+  //    console.error("[setup] User module failed to initialize, stopping.", e);
+  //    return;
+  // }
 
-  if (event.type === "IMPORT_USERS" && event.body) {
+  // if (event.type === "IMPORT_USERS" && event.body) {
 
-    const users = event.body.split("\n");
+  //   const users = event.body.split("\n");
 
-    let c = 0;
-    for (const user of users) {
-      c++;
-      if (c === 1) {
-        continue;
-      }
-      let fields = user.split(",");
-      if (fields.length < 7) {
-        continue;
-      }
-      fields = fields.map(f => f.trim())
-      // (name: string, companyRole: CompanyRole, taxId: string, password: string, email: string, userRole: string
-      const cr =
-        fields[4] == "Government"
-          ? CompanyRole.GOVERNMENT
-          : fields[4] == "Certifier"
-          ? CompanyRole.CERTIFIER
-          : fields[4] == "API"
-          ? CompanyRole.API
-          :CompanyRole.PROGRAMME_DEVELOPER;
-      const ur =
-        fields[5] == "admin"
-          ? Role.Admin
-          : fields[5] == "Manager"
-          ? Role.Manager
-          : Role.ViewOnly;
-      console.log('Inserting user', fields[0],
-      cr,
-      fields[3],
-      fields[1],
-      ur,
-      fields[2])
-      try {
-        await userService.createUserWithPassword(
-          fields[0],
-          cr,
-          fields[3],
-          fields[6],
-          fields[1],
-          ur,
-          fields[2],
-          (cr === CompanyRole.API && fields.length > 7) ? fields[7] : undefined
-        );
-      } catch (e) {
-        console.log('Fail to create user', fields[1], e)
-      }
+  //   let c = 0;
+  //   for (const user of users) {
+  //     c++;
+  //     if (c === 1) {
+  //       continue;
+  //     }
+  //     let fields = user.split(",");
+  //     if (fields.length < 7) {
+  //       continue;
+  //     }
+  //     fields = fields.map(f => f.trim())
+  //     // (name: string, companyRole: CompanyRole, taxId: string, password: string, email: string, userRole: string
+  //     const cr =
+  //       fields[4] == "Government"
+  //         ? CompanyRole.GOVERNMENT
+  //         : fields[4] == "Certifier"
+  //         ? CompanyRole.CERTIFIER
+  //         : fields[4] == "API"
+  //         ? CompanyRole.API
+  //         :CompanyRole.PROGRAMME_DEVELOPER;
+  //     const ur =
+  //       fields[5] == "admin"
+  //         ? Role.Admin
+  //         : fields[5] == "Manager"
+  //         ? Role.Manager
+  //         : Role.ViewOnly;
+  //     console.log('Inserting user', fields[0],
+  //     cr,
+  //     fields[3],
+  //     fields[1],
+  //     ur,
+  //     fields[2])
+  //     try {
+  //       await userService.createUserWithPassword(
+  //         fields[0],
+  //         cr,
+  //         fields[3],
+  //         fields[6],
+  //         fields[1],
+  //         ur,
+  //         fields[2],
+  //         (cr === CompanyRole.API && fields.length > 7) ? fields[7] : undefined
+  //       );
+  //     } catch (e) {
+  //       console.log('Fail to create user', fields[1], e)
+  //     }
      
-    }
-    return;
-  }
+  //   }
+  //   return;
+  // }
 
-  if (event.type === "IMPORT_ORG" && event.body) {
-    const companyApp = await NestFactory.createApplicationContext(
-      CompanyModule,
-      {
-        logger: getLogger(CompanyModule),
-      }
-    );
-    const companyService = companyApp.get(CompanyService);
-    const configService = companyApp.get(ConfigService);
+  // if (event.type === "IMPORT_ORG" && event.body) {
+  //   const companyApp = await NestFactory.createApplicationContext(
+  //     CompanyModule,
+  //     {
+  //       logger: getLogger(CompanyModule),
+  //     }
+  //   );
+  //   const companyService = companyApp.get(CompanyService);
+  //   const configService = companyApp.get(ConfigService);
 
-    const companies = event.body.split("\n");
+  //   const companies = event.body.split("\n");
 
-    let c = 0;
-    for (const company of companies) {
-      c++;
-      if (c === 1) {
-        continue;
-      }
-      let fields = company.split(",");
-      if (fields.length < 5) {
-        continue;
-      }
-      fields = fields.map(f => f.trim())
-      // (name: string, companyRole: CompanyRole, taxId: string, password: string, email: string, userRole: string
-      const cr = fields[4] == "Certifier"
-          ? CompanyRole.CERTIFIER
-          : fields[4] == "API"
-          ? CompanyRole.API
-          : CompanyRole.PROGRAMME_DEVELOPER;
+  //   let c = 0;
+  //   for (const company of companies) {
+  //     c++;
+  //     if (c === 1) {
+  //       continue;
+  //     }
+  //     let fields = company.split(",");
+  //     if (fields.length < 5) {
+  //       continue;
+  //     }
+  //     fields = fields.map(f => f.trim())
+  //     // (name: string, companyRole: CompanyRole, taxId: string, password: string, email: string, userRole: string
+  //     const cr = fields[4] == "Certifier"
+  //         ? CompanyRole.CERTIFIER
+  //         : fields[4] == "API"
+  //         ? CompanyRole.API
+  //         : CompanyRole.PROGRAMME_DEVELOPER;
 
-      try {
-        const org = await companyService.create({
-              taxId: fields[3],
-              companyId: undefined,
-              name: fields[0],
-              email: fields[1],
-              phoneNo: fields[2],
-              website: undefined,
-              address: configService.get("systemCountryName"),
-              logo: undefined,
-              country: configService.get("systemCountry"),
-              companyRole: cr,
-              createdTime: undefined,
-            });
-        console.log('Company created', org)
-      } catch (e) {
-        console.log('Fail to create company', fields[1])
-      }
-    }
-    return;
-  }
+  //     try {
+  //       const org = await companyService.create({
+  //             taxId: fields[3],
+  //             companyId: undefined,
+  //             name: fields[0],
+  //             email: fields[1],
+  //             phoneNo: fields[2],
+  //             website: undefined,
+  //             address: configService.get("systemCountryName"),
+  //             logo: undefined,
+  //             country: configService.get("systemCountry"),
+  //             companyRole: cr,
+  //             createdTime: undefined,
+  //           });
+  //       console.log('Company created', org)
+  //     } catch (e) {
+  //       console.log('Fail to create company', fields[1])
+  //     }
+  //   }
+  //   return;
+  // }
 
-  if (event.type === "UPDATE_COORDINATES") {
-    const prApp = await NestFactory.createApplicationContext(ProgrammeModule, {
-      logger: getLogger(ProgrammeModule),
-    });
-    const programmeService = prApp.get(ProgrammeService);
-    await programmeService.regenerateRegionCoordinates();
-    return;
-  }
+  // if (event.type === "UPDATE_COORDINATES") {
+  //   const prApp = await NestFactory.createApplicationContext(ProgrammeModule, {
+  //     logger: getLogger(ProgrammeModule),
+  //   });
+  //   const programmeService = prApp.get(ProgrammeService);
+  //   await programmeService.regenerateRegionCoordinates();
+  //   return;
+  // }
 
-  const rootUser = await userService.findOne(event["rootEmail"]);
-  if (rootUser != undefined) {
-    console.warn("[setup] Root user %s already created and setup is completed", event["rootEmail"]);
-  }
+  // const rootUser = await userService.findOne(event["rootEmail"]);
+  // if (rootUser != undefined) {
+  //   console.warn("[setup] Root user %s already created and setup is completed", event["rootEmail"]);
+  // }
 
-  let ledgerDBInterface: LedgerDBInterface;
+  // let ledgerDBInterface: LedgerDBInterface;
 
-  try {
-    const app = await NestFactory.createApplicationContext(LedgerDbModule, {
-      logger: getLogger(LedgerDbModule),
-    });
+  // try {
+  //   const app = await NestFactory.createApplicationContext(LedgerDbModule, {
+  //     logger: getLogger(LedgerDbModule),
+  //   });
 
-    ledgerDBInterface = app.get(LedgerDBInterface);
-    console.log(`[setup] Ledger module initialized`);
-  } catch (e) {
-    console.error("[setup] Ledger module failed to initialize, stopping.", e);
-    return;
-  }
+  //   ledgerDBInterface = app.get(LedgerDBInterface);
+  //   console.log(`[setup] Ledger module initialized`);
+  // } catch (e) {
+  //   console.error("[setup] Ledger module failed to initialize, stopping.", e);
+  //   return;
+  // }
   
-  try {
-    await ledgerDBInterface.createTable("company");
-    await ledgerDBInterface.createIndex("txId", "company");
+  // try {
+  //   await ledgerDBInterface.createTable("company");
+  //   await ledgerDBInterface.createIndex("txId", "company");
 
-    await ledgerDBInterface.createTable("overall");
-    await ledgerDBInterface.createIndex("txId", "overall");
-    const creditOverall = new CreditOverall();
-    creditOverall.credit = 0;
-    creditOverall.txId = event["systemCountryCode"];
-    creditOverall.txRef = "genesis block";
-    creditOverall.txType = TxType.ISSUE;
-    await ledgerDBInterface.insertRecord(creditOverall, "overall");
-    await ledgerDBInterface.createTable();
-    await ledgerDBInterface.createIndex("programmeId");
-    console.info("[setup] QLDB Table company created", );
-  } catch (e) {
-    console.error("[setup] QLDB table does not create", e);
-  }
+  //   await ledgerDBInterface.createTable("overall");
+  //   await ledgerDBInterface.createIndex("txId", "overall");
+  //   const creditOverall = new CreditOverall();
+  //   creditOverall.credit = 0;
+  //   creditOverall.txId = event["systemCountryCode"];
+  //   creditOverall.txRef = "genesis block";
+  //   creditOverall.txType = TxType.ISSUE;
+  //   await ledgerDBInterface.insertRecord(creditOverall, "overall");
+  //   await ledgerDBInterface.createTable();
+  //   await ledgerDBInterface.createIndex("programmeId");
+  //   console.info("[setup] QLDB Table company created", );
+  // } catch (e) {
+  //   console.error("[setup] QLDB table does not create", e);
+  // }
 
-  try {
-    const company = new OrganisationDto();
-    company.country = event["systemCountryCode"];
-    company.name = event["name"];
-    company.logo = event["logoBase64"];
-    company.companyRole = CompanyRole.GOVERNMENT;
+  // try {
+  //   const company = new OrganisationDto();
+  //   company.country = event["systemCountryCode"];
+  //   company.name = event["name"];
+  //   company.logo = event["logoBase64"];
+  //   company.companyRole = CompanyRole.GOVERNMENT;
 
-    const user = new UserDto();
-    user.email = event["rootEmail"];
-    user.name = "Root";
-    user.role = Role.Root;
-    user.phoneNo = "-";
-    user.company = company;
+  //   const user = new UserDto();
+  //   user.email = event["rootEmail"];
+  //   user.name = "Root";
+  //   user.role = Role.Root;
+  //   user.phoneNo = "-";
+  //   user.company = company;
 
-    console.log("[setup] Adding company", company);
-    console.log("[setup] Adding user", user);
+  //   console.log("[setup] Adding company", company);
+  //   console.log("[setup] Adding user", user);
 
-    await userService.create(user, -1, CompanyRole.GOVERNMENT);
-  } catch (e) {
-    console.error(`[setup] User ${event["rootEmail"]} failed to create`, e);
-  }
+  //   await userService.create(user, -1, CompanyRole.GOVERNMENT);
+  // } catch (e) {
+  //   console.error(`[setup] User ${event["rootEmail"]} failed to create`, e);
+  // }
 
-  try {
-    console.log("[setup] Adding countryData");
-    const countryData = fs.readFileSync("countries.json", "utf8");
-    const jsonCountryData = JSON.parse(countryData);
-    const utils = await NestFactory.createApplicationContext(UtilModule);
-    const countryService = utils.get(CountryService);
+  // try {
+  //   console.log("[setup] Adding countryData");
+  //   const countryData = fs.readFileSync("countries.json", "utf8");
+  //   const jsonCountryData = JSON.parse(countryData);
+  //   const utils = await NestFactory.createApplicationContext(UtilModule);
+  //   const countryService = utils.get(CountryService);
   
-    jsonCountryData.forEach(async (countryItem) => {
-      if (countryItem["UN Member States"] === "x") {
-        const country = new Country();
-        country.alpha2 = countryItem["ISO-alpha2 Code"];
-        country.alpha3 = countryItem["ISO-alpha3 Code"];
-        country.name = countryItem["English short"];
-        await countryService.insertCountry(country);
-      }
-    });
-  } catch (e) {
-    console.error("[setup] Failed to create country data, stopping", e);
-    return;
-  }
+  //   jsonCountryData.forEach(async (countryItem) => {
+  //     if (countryItem["UN Member States"] === "x") {
+  //       const country = new Country();
+  //       country.alpha2 = countryItem["ISO-alpha2 Code"];
+  //       country.alpha3 = countryItem["ISO-alpha3 Code"];
+  //       country.name = countryItem["English short"];
+  //       await countryService.insertCountry(country);
+  //     }
+  //   });
+  // } catch (e) {
+  //   console.error("[setup] Failed to create country data, stopping", e);
+  //   return;
+  // }
 
   try {
     const locationApp = await NestFactory.createApplicationContext(
--- backend/services/src/shared/location/file.location.service.ts
+++ backend/services/src/shared/location/file.location.service.ts
@@ -1,46 +1,75 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { resolve } from "path";
 import { LocationInterface } from "./location.interface";
-const fs = require('fs')
+import { Region } from "../entities/region.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { readFileSync } from "fs";
 
 @Injectable()
 export class FileLocationService implements LocationInterface {
 
-  private regionMap: { [key: string]: number[] } = {}
+  private regionMap: Array<{ name: string, location: number[], countryCode: string }>  = [];
+
   constructor(
-    private logger: Logger
+    private logger: Logger,
+    @InjectRepository(Region) private regionRepo: Repository<Region>
   ) {
     const deliminator = ','
-    const regionRawData = fs.readFileSync('regions.csv', 'utf8');
+    const regionRawData = readFileSync('regions.csv', 'utf8');
     const headers = regionRawData.slice(0, regionRawData.indexOf("\n")).split(deliminator).map(e => e.trim().replace('\r', ''))
     const rows = regionRawData.slice(regionRawData.indexOf("\n") + 1).split("\n")
 
     const nameIndex = headers.indexOf('Name')
+    const countryCodeIndex = headers.indexOf('CountryCode')
     const latitudeIndex = headers.indexOf('Latitude')
     const longitudeIndex = headers.indexOf('Longitude')
-    console.log(headers, nameIndex, latitudeIndex, longitudeIndex)
-    if (nameIndex >=0 && latitudeIndex >=0 && longitudeIndex >=0) {
+    console.log(headers, nameIndex, countryCodeIndex, latitudeIndex, longitudeIndex)
+    if (nameIndex >=0 && latitudeIndex >=0 && longitudeIndex >=0 && countryCodeIndex >=0) {
       for (let row of rows) {
         row = row.replace('\r', '')
         const columns = row.split(deliminator)
         if (columns.length != headers.length) {
           continue
         }
-        this.regionMap[columns[nameIndex].trim()] = [ Number(columns[longitudeIndex].trim()), Number(columns[latitudeIndex].trim()) ]
+        this.regionMap.push({
+          name: columns[nameIndex].trim(),
+          countryCode: columns[countryCodeIndex].trim(),
+          location: [ Number(columns[longitudeIndex].trim()), Number(columns[latitudeIndex].trim()) ]
+        })
       }
     }
 
-    this.logger.log(`Regions loaded: ${Object.values(this.regionMap).length}`)
+    this.logger.log(`Regions loaded: ${this.regionMap.length}`)
+    logger.log('Constructor initialized', 'FileLocationService');
   }
   
-  public init(): Promise<void> {
-    return null;
+  public async init(): Promise<void> {
+    this.logger.log('Started', 'init')
+    const existingRegionsCount = await this.regionRepo.count();
+
+    if (existingRegionsCount <= 0){
+      const data: Region[] = [];
+      for (const rg of this.regionMap) {
+        const region = new Region();
+        region.countryAlpha2 = rg.countryCode;
+        region.regionName = rg.name;
+        region.geoCoordinates = rg.location;
+        region.lang = 'en';
+        region.key = `${region.lang}-${rg.countryCode}-${rg.name}`;
+        data.push(region);
+      }
+
+      await this.regionRepo.save(data);
+      this.logger.log(`Regions loaded count: ${data.length}`, 'init');
+    } else {
+      this.logger.log(`Skipped adding regions.`, 'init')
+    }
   }
 
   public getCoordinatesForRegion(regions: string[]): Promise<number[][]> {
     return new Promise((resolve, reject) => {
       resolve(regions.map( (region) => {
-        return this.regionMap[region]
+        return this.regionMap.find(x => x.name === region).location
       }));
     })
   }
