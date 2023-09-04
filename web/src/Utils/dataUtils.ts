f6f8dd515771ed650ac896b38736923ca494c511
igor-perlinnet <igor@perlin.net>
2023-09-04 06:08:24 +0300

    Update data utils

--- web/src/Utils/dataUtils.ts
+++ web/src/Utils/dataUtils.ts
@@ -1,4 +1,4 @@
-export function isBase64(str: string) {
+export const isBase64 = (str: string) => {
   if (str === '' || str.trim() === '') {
     return false;
   }
@@ -7,4 +7,24 @@ export function isBase64(str: string) {
   } catch (err) {
     return false;
   }
-}
+};
+
+export const base64toBlob = (data: string) => {
+  // Cut the prefix `data:application/pdf;base64` from the raw base 64
+  const base64WithoutPrefix = data.substr('data:application/pdf;base64,'.length);
+
+  const bytes = atob(base64WithoutPrefix);
+  let length = bytes.length;
+  const out = new Uint8Array(length);
+
+  while (length--) {
+    out[length] = bytes.charCodeAt(length);
+  }
+
+  return new Blob([out], { type: 'application/pdf' });
+};
+
+export const getBase64ObjectUrl = (base64String: string) => {
+  const blob = base64toBlob(base64String);
+  return URL.createObjectURL(blob);
+};
