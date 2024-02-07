669d1434d1495cc4796a107e0f1e522df6f8fbfb
mblishch <mykhaylo.blishch@gmail.com>
2024-02-07 12:13:51 +0200

    Use custom page for company creation

--- web/src/Pages/Company/addNewCompany.tsx
+++ web/src/Pages/Company/addNewCompany.tsx
@@ -2,7 +2,7 @@ import { useConnection } from '../../Context/ConnectionContext/connectionContext
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useUserContext } from '../../Context/UserInformationContext/userInformationContext';
-import { AddNewCompanyComponent } from '@undp/carbon-library';
+import { AddNewCompanyComponent } from './AddNewCompanyComponentUndp';
 
 const AddNewCompany = () => {
   const navigate = useNavigate();
